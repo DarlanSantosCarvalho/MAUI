2d330e1d53e1519ec73c5f6490e5831c9a6e3a39
Darlan Carvalho <92957575+DarlanSantosCarvalho@users.noreply.github.com>
2024-09-05 09:58:30 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count % 2 == 0)
+                CounterBtn.Text = $"Você clicou {count} vezes, um número par";
+            else if (count % 2 != 0)
+                CounterBtn.Text = $"Você clicou {count} vezes, um número ímpar";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
